d79481389d97f5056b3d8c624a52ffebb81f06d5
Nikolas Andreou <nickandreou94@gmail.com>
2023-03-03 20:46:09 +0200

    Update project to enforce C# 11 standards

--- Streamliner/Fluent/Flow/FluentFlowDefinition.cs
+++ Streamliner/Fluent/Flow/FluentFlowDefinition.cs
@@ -9,10 +9,7 @@ public class FluentFlowDefinition
     private readonly FlowType _flowType;
     private uint _iterations = 1;
 
-    internal FluentFlowDefinition(FlowType flowType)
-    {
-        _flowType = flowType;
-    }
+    internal FluentFlowDefinition(FlowType flowType) => _flowType = flowType;
 
     public FluentFlowDefinition WithIterations(uint iterations)
     {
