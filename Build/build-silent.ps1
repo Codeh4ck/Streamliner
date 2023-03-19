b76839904ac51c5b63b4274e5bbfbe2dc04f86c7
Nikolas Andreou <nickandreou94@gmail.com>
2023-03-20 01:08:57 +0200

    Set build scripts to build with Release configuration

--- Build/build-silent.ps1
+++ Build/build-silent.ps1
@@ -1,2 +1,3 @@
 Set-Location ../
-dotnet build ./Streamliner.sln
+dotnet build ./Streamliner.sln --configuration Release
+dotnet pack ./Streamliner.sln --configuration Release
--- Build/build.ps1
+++ Build/build.ps1
@@ -1,5 +1,6 @@
 Set-Location ../
-dotnet build ./Streamliner.sln
+dotnet build ./Streamliner.sln --configuration Release
+dotnet pack ./Streamliner.sln --configuration Release
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
