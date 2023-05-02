3a2ac49282b2d1c3cd4746d7a1e9356bf7eabb83
Nikolas Andreou <nickandreou94@gmail.com>
2023-05-03 00:51:56 +0300

    Add message queue factory class to work as an adapter between a message queue and Streamliner

---
+++ Streamliner/Core/Links/Remote/MessageQueues/IMqFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Streamliner.Core.Links.Remote.MessageQueues
+{
+    public interface IMqFactory
+    {
+        IMqConsumer<T> CreateConsumer<T>(string queueName);
+        IMqProducer CreateProducer(string exchangeName);
+        
+        void PurgeQueue(string queueName);
+        void BindQueue(BindQueueArgs args);
+        void CreateQueue(CreateQueueArgs args);
+        void CreateExchange(CreateExchangeArgs args);
+    }
+}
