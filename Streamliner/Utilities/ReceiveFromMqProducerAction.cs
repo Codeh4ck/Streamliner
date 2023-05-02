02699c100967ed33a722527b2e3f3ebdbbc8894e
Nikolas Andreou <nickandreou94@gmail.com>
2023-05-03 00:53:53 +0300

    Add default producer/consumer actions that receive/send data from/to message queues

---
+++ Streamliner/Utilities/ReceiveFromMqProducerAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using Streamliner.Actions;
+using Streamliner.Core.Links.Remote.MessageQueues;
+
+namespace Streamliner.Utilities
+{
+    public abstract class ReceiveFromMqProducerAction<T> : ProducerBlockActionBase<T>
+    {
+        private IMqConsumer<T> _consumer;
+        private readonly IMqFactory _mqFactory;
+        protected abstract InitQueueArgs InitiateQueueArgs { get; }
+
+        protected ReceiveFromMqProducerAction(IMqFactory mqFactory) => 
+            _mqFactory = mqFactory ?? throw new ArgumentNullException(nameof(mqFactory));
+
+        public override bool TryProduce(out T model, CancellationToken token = default)
+        {
+            model = _consumer.Consume(token);
+            return true;
+        }
+        
+        protected override void OnStart(object context = null)
+        {
+            base.OnStart(context);
+            
+            InitQueueArgs initQueueArgs = InitiateQueueArgs;
+            
+            _mqFactory.CreateQueue(initQueueArgs.CreateQueueArgs);
+            _mqFactory.CreateExchange(initQueueArgs.CreateExchangeArgs);
+            _mqFactory.BindQueue(initQueueArgs.BindQueueArgs);
+            
+            _consumer = _mqFactory.CreateConsumer<T>(initQueueArgs.CreateQueueArgs.QueueName);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            if (_consumer == null) return;
+            
+            _consumer.Dispose();
+            _consumer = null;
+        }
+    }
+}
---
+++ Streamliner/Utilities/SendToMqConsumerAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using Streamliner.Actions;
+using Streamliner.Core.Links.Remote.MessageQueues;
+
+namespace Streamliner.Utilities
+{
+    public abstract class SendToMqConsumerAction<T> : ConsumerBlockActionBase<T>
+    {
+        private IMqProducer _producer;
+        private readonly IMqFactory _mqFactory;
+
+        private readonly string _routingKey;
+        private readonly string _exchangeName;
+        protected abstract CreateExchangeArgs ExchangeArgs { get; }
+
+        protected SendToMqConsumerAction(string exchangeName, IMqFactory mqFactory)
+        {
+            _mqFactory = mqFactory ?? throw new ArgumentNullException(nameof(mqFactory));
+            _exchangeName = exchangeName ?? throw new ArgumentNullException(nameof(exchangeName));
+        }
+
+        protected SendToMqConsumerAction(string routingKey, string exchangeName, IMqFactory mqFactory) : this(
+            exchangeName, mqFactory)
+            => _routingKey = routingKey ?? throw new ArgumentNullException(nameof(routingKey));
+
+        public override void Consume(T model, CancellationToken token = default) =>
+            _producer.Produce<T>(model, _routingKey, token);
+
+        protected override void OnStart(object context = null)
+        {
+            base.OnStart(context);
+            _mqFactory.CreateExchange(ExchangeArgs);
+            _producer = _mqFactory.CreateProducer(_exchangeName);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            if (_producer == null) return;
+
+            _producer.Dispose();
+            _producer = null;
+        }
+    }
+}
