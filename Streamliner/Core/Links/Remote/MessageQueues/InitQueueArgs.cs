c375ac751e084a53147be66df137f92707ff1ade
Nikolas Andreou <nickandreou94@gmail.com>
2023-05-03 00:52:05 +0300

    Add init queue args model

---
+++ Streamliner/Core/Links/Remote/MessageQueues/InitQueueArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Streamliner.Core.Links.Remote.MessageQueues
+{
+    public class InitQueueArgs
+    {
+        public BindQueueArgs BindQueueArgs { get; set; }
+        public CreateQueueArgs CreateQueueArgs { get; set; }
+        public CreateExchangeArgs CreateExchangeArgs { get; set; }
+    }
+}
