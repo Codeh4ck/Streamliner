57bcff81a92d05a4dfcab79bd83032f373b95199
Nikolas Andreou <nickandreou94@gmail.com>
2023-05-03 00:51:36 +0300

    Add generic message queue producer/consumers to be used internally

---
+++ Streamliner/Core/Links/Remote/MessageQueues/IMqConsumer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Streamliner.Core.Links.Remote.MessageQueues
+{
+    public interface IMqConsumer<T> : IDisposable
+    {
+        T Consume(CancellationToken token = default);
+        Task<T> ConsumeAsync(CancellationToken token = default);
+    }
+}
---
+++ Streamliner/Core/Links/Remote/MessageQueues/IMqProducer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Streamliner.Core.Links.Remote.MessageQueues
+{
+    public interface IMqProducer : IDisposable
+    {
+        void Produce<T>(T model, string queueName = null, CancellationToken token = default);
+        void Produce<T>(IEnumerable<T> models, string queueName = null, CancellationToken token = default);
+        Task ProduceAsync<T>(T model, string queueName = null, CancellationToken token = default);
+        Task ProduceAsync<T>(IEnumerable<T> models, string queueName = null, CancellationToken token = default);
+    }
+}
