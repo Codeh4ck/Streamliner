4526bd6bebd82ecafa26b732a07fe8d967d8fecd
Nikolas Andreou <nickandreou94@gmail.com>
2023-05-03 00:52:42 +0300

    Implement remote message queue link receiver

---
+++ Streamliner/Core/Links/Remote/RemoteMqLinkReceiver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using Streamliner.Core.Links.Remote.MessageQueues;
+
+namespace Streamliner.Core.Links.Remote
+{
+    public class RemoteMqLinkReceiver<T> : IBlockLinkReceiver<T>
+    {
+        private readonly IMqConsumer<T> _consumer;
+
+        public RemoteMqLinkReceiver(IMqConsumer<T> consumer)
+        {
+            _consumer = consumer ?? throw new ArgumentNullException(nameof(consumer));
+        }
+        
+        public T Receive(CancellationToken token = default) => _consumer.Consume();
+    }
+}
